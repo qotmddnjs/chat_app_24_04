98b243affe15134e788b7ba4122a99ef2fef754c
qotmddnjs <dudcnsaus@gmail.com>
2024-04-11 16:29:34 +0900

    메세지를 리스트에 저장

--- src/main/java/com/koreat/exam/chat_app_24_04/ChatController.java
+++ src/main/java/com/koreat/exam/chat_app_24_04/ChatController.java
@@ -1,14 +1,37 @@
 package com.koreat.exam.chat_app_24_04;
 
+
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/chat")
 public class ChatController {
-  @PostMapping ("/writeMessage")
-  @ResponseBody
-  public String writeMessage() {
-    return "메세지 작성됨";
+  private List<ChatMessage> chatMessages = new ArrayList<>();
+
+  public record writeMessageResponse(long id) {
+
   }
+
+  @PostMapping("/writeMessage")
+  @ResponseBody
+  public RsData<writeMessageResponse> writeMessage() {
+
+    ChatMessage message = new ChatMessage("홍길동", "안녕하세요");
+
+    chatMessages.add(message);
+
+    return new RsData(
+        "S-1",
+        "메세지가 작성됨",
+        new writeMessageResponse(message.getId()
+        )
+    );
+  }
+
+
 }
---
+++ src/main/java/com/koreat/exam/chat_app_24_04/ChatMessage.java
@@ -0,0 +1,31 @@
+package com.koreat.exam.chat_app_24_04;
+
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Getter
+public class ChatMessage {
+  private long id;
+  private LocalDateTime createTime;
+  private String authorName;
+  private String content;
+
+  public ChatMessage(String authorName, String content) {
+    this(ChatMessageIdGenerator.getNextId(),LocalDateTime.now(),authorName,content);
+  }
+
+}
+
+class ChatMessageIdGenerator{
+  public static long id = 0;
+
+  public static long getNextId(){
+    return ++id;
+  }
+}
